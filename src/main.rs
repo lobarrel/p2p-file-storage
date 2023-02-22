e103a4bdb782f7fb9e703ac4cad26cddf0c485a0
lobarrel <loribottelli@gmail.com>
2023-02-22 12:20:16 +0100

    read and write file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::fs::File;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut f = File::open("./data.txt").await?;
+    let mut buffer = Vec::new();
+
+    // read the whole file
+    f.read_to_end(&mut buffer).await?;
+    println!("{:?}", buffer);
+
+    let mut f2 = File::create("./output.txt").await?;
+    f2.write_all(&mut buffer).await?;
+    Ok(())
 }
