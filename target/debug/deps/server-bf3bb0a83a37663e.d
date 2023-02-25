3e1f896090bb8aeb19e10d09eca9a10e832c33d6
lobarrel <loribottelli@gmail.com>
2023-02-25 10:20:16 +0100

    changed server to coordinator

---
+++ target/debug/deps/server-bf3bb0a83a37663e.d
@@ -0,0 +1,5 @@
+/Users/lorenzobottelli/Desktop/Rust/p2p-file-storage/target/debug/deps/server-bf3bb0a83a37663e.rmeta: coordinator/src/main.rs
+
+/Users/lorenzobottelli/Desktop/Rust/p2p-file-storage/target/debug/deps/server-bf3bb0a83a37663e.d: coordinator/src/main.rs
+
+coordinator/src/main.rs:
